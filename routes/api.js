e7a513ec34ef4fe71d074bb906f9243dc7b02f7e
Cecilia <earwen1986@gmail.com>
2021-04-28 17:17:09 +0100

    Created route to test getNum method

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
-
+  app.get("/api/convert", (req, res) => {
+    console.log(convertHandler.getNum(req.body.test));
+    res.send("ok");
+  });
 };
